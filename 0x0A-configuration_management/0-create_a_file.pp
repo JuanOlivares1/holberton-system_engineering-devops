bd5cbbfa843ba81e31e9fe2f096ca86a81b8e953
Juan Olivares <jdop2000@gmail.com>
2020-08-03 23:28:02 +0000

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file {'/tmp/holberton':
-  path    => '/tmp/holberton',
+  ensure  => 'present',
   mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  owner   => www-data,
+  group   => www-data,
+  content => 'I love Puppet'
 }
