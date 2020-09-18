991de9be3e70f428eb162804f939cc6e6f3a5265
Juan Olivares <jdop2000@gmail.com>
2020-09-18 05:46:49 +0000

    top ten

---
+++ 0x16-api_advanced/1-top-ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Request data to Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """get the top ten hot post"""
+    url = 'https://www.reddit.com/r/' + subreddit + "/hot.json"
+    head = {'User-Agent': 'valaak'}
+    response = requests.get(url, headers=head, allow_redirects=False)
+    if response.status_code == 200:
+        json = response.json()
+        if 'data' in json:
+            for post in json.get("data").get("children"):
+                print(post.get("data").get("title"))
+        else:
+            print(None)
+    else:
+        print(None)
+        return(0)
