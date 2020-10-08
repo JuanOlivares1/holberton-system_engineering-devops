fcdd49cdfac9217a5a7202796d93b524e35b5779
Juan Olivares <jdop2000@gmail.com>
2020-10-08 19:23:16 +0000

    refactoring manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # setting server to deal with many requests
 
-exec {'fixing-nginx':
+exec {'fix--for-nginx':
     command => "sed -i 's/15/1000/g' /etc/default/nginx",
     path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 
-exec {'restarting-nginx':
+exec {'arestart-nginx':
     command => 'service nginx restart',
     path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
